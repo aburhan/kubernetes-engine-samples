1f7059e34041ce76b082fe4f2ed685637f52688b
aburhan <ameenah.burhan@gmail.com>
2023-03-07 18:54:21 +0000

    sync

--- gke-vpa-recommendations/scripts/deploy_pipeline.sh
+++ gke-vpa-recommendations/scripts/deploy_pipeline.sh
@@ -1,25 +1,29 @@
 
-echo "Create a service account to run the pipeline"
+1. Create a service account to run the pipeline
+```sh
 gcloud iam service-accounts create mql-export-metrics \
 --display-name "MQL export metrics SA" \
 --description "Used for the function that export monitoring metrics"
+```
 
-echo "Assigning IAM roles to the service account..."
+1. Assigning IAM roles to the service accounts
+```sh
 gcloud projects add-iam-policy-binding  $PROJECT_ID --member="serviceAccount:mql-export-metrics@$PROJECT_ID.iam.gserviceaccount.com" --role="roles/monitoring.viewer"
 gcloud projects add-iam-policy-binding  $PROJECT_ID --member="serviceAccount:mql-export-metrics@$PROJECT_ID.iam.gserviceaccount.com" --role="roles/bigquery.dataEditor"
 gcloud projects add-iam-policy-binding  $PROJECT_ID --member="serviceAccount:mql-export-metrics@$PROJECT_ID.iam.gserviceaccount.com" --role="roles/bigquery.dataOwner"
 gcloud projects add-iam-policy-binding  $PROJECT_ID --member="serviceAccount:mql-export-metrics@$PROJECT_ID.iam.gserviceaccount.com" --role="roles/bigquery.jobUser"
 gcloud projects add-iam-policy-binding  $PROJECT_ID --member="serviceAccount:mql-export-metrics@$PROJECT_ID.iam.gserviceaccount.com" --role="roles/run.invoker"
+```
 
-echo "Creating the Pub/Sub topic..."
-gcloud pubsub topics create metric_export
-
-
-echo "Build image and store the image in Artifact registry..."
+1. Build image and store the image in Artifact registry
+```sh
 gcloud builds submit metrics-exporter --config=metrics-exporter/cloudbuild.yaml  --substitutions=_REGION=$REGION
 
+```
 
-echo "Deploy the Cloud Run Job.."
+
+1. Deploy the Cloud Run Job
+```sh
 gcloud beta run jobs deploy metric-exporter \
     --image=$REGION-docker.pkg.dev/$PROJECT_ID/main/metric-exporter \
     --set-env-vars=PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python \
@@ -30,16 +34,15 @@ gcloud beta run jobs deploy metric-exporter \
     --parallelism=0 \
     --service-account=mql-export-metrics@$PROJECT_ID.iam.gserviceaccount.com \
     --region=$REGION
+```
 
-echo "Enable the Cloud Scheduler api.."
-gcloud services enable cloudscheduler.googleapis.com
-
-
+1. Create a scheduled job to run the Cloud Run job
+```sh
 gcloud scheduler jobs create http metric-exporter \
   --location $REGION \
   --schedule="0 23 * * *" \
   --uri="https://$REGION-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/$PROJECT_ID/jobs/metric-exporter:run" \
   --http-method POST \
   --oauth-service-account-email "mql-export-metrics@$PROJECT_ID.iam.gserviceaccount.com"
+```
 
-echo "Deployment complete"
