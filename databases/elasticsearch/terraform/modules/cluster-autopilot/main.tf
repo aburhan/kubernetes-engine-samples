26b372fe329ff5e91c25bd2e80796882a648e32d
Ameenah Burhan <ameenah.burhan@gmail.com>
2024-03-25 18:18:16 -0400

    Update main.tf

--- databases/elasticsearch/terraform/modules/cluster-autopilot/main.tf
+++ databases/elasticsearch/terraform/modules/cluster-autopilot/main.tf
@@ -14,22 +14,23 @@
 
 # [START gke_databases_elasticsearch_autopilot_private_regional_cluster]
 module "elasticsearch_cluster" {
-  source                   = "terraform-google-modules/kubernetes-engine/google//modules/beta-autopilot-private-cluster"
-  version                  = "~> 29.0"
-  project_id               = var.project_id
-  name                     = "${var.cluster_prefix}-cluster"
-  regional                 = true
-  region                   = var.region
-  network                  = var.network
-  subnetwork               = var.subnetwork
-  ip_range_pods            = "k8s-pod-range"
-  ip_range_services        = "k8s-service-range"
-  create_service_account   = true
-  enable_private_endpoint  = false
-  enable_private_nodes     = true
-  master_ipv4_cidr_block   = "172.16.0.0/28"
-  enable_cost_allocation = true
-  deletion_protection = false
+  source                  = "terraform-google-modules/kubernetes-engine/google//modules/beta-autopilot-private-cluster"
+  version                 = "~> 29.0"
+  project_id              = var.project_id
+  name                    = "${var.cluster_prefix}-cluster"
+  regional                = true
+  region                  = var.region
+  network                 = var.network
+  subnetwork              = var.subnetwork
+  ip_range_pods           = "k8s-pod-range"
+  ip_range_services       = "k8s-service-range"
+  create_service_account  = true
+  enable_private_endpoint = false
+  enable_private_nodes    = true
+  master_ipv4_cidr_block  = "172.16.0.0/28"
+  enable_cost_allocation  = true
+  deletion_protection     = false
+  release_channel         = "RAPID"
 
   cluster_resource_labels = {
     name      = "${var.cluster_prefix}-cluster"
